c55c2458062a583ff1d163e9a4d7beeb8e10202d
Michael <mikahado@gmail.com>
2022-08-30 01:03:53 -0400

    arithmetic completed

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+multiply
 
-// Write your code here
+const num3 = 10
+const num4 = 6
+const mod = num3 % num4
+mod
+
+const max = Math.max(2,4,14,20)
+max
+
+const random = Math.floor(Math.random() > 0)
+random
